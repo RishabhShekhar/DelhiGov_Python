06d4efbd638a1bf348c89ce8c1fdb5dc6c27a9e6
RishabhShekhar <rishabhshekhar04@gmail.com>
2021-02-03 14:58:07 +0530

    Day_1

---
+++ Day_1.py
@@ -0,0 +1,192 @@
+# print("Rishabh Shekhar")
+# print("My age is 23")
+
+# String: "Rishabh Shekhar uygfefkwiflgiegujkbjkrv"
+# Integer: 43324 234 24235 454
+# Float: 623.234 22.454
+# Boolean: True False
+
+# print(1 == 1) #Comparing 1 to 1
+
+# print(2 == 3)
+
+'''
+vhckavka
+ascljhakucha
+kucgaic
+iashcoabcol
+'''
+
+# print(type(1.0))
+# print(type(1))
+# print(type("tdrrytwqdfwy"))
+# print(type(True))
+
+
+# a = "Rishabh"
+# print(type(a))
+# print(a)
+
+# A = "Shekhar"
+# print(A)
+
+# name = "Rishabh"
+# age = 23
+# # print("My name is "+ name) #concatenation
+# print("My age is "+ str(age)) 
+
+# str()
+# int()
+# float()
+
+#typecasting
+
+# name = "Rishabh"
+# print("My name is", name)
+
+# print(2+5)
+# print(2*5)
+# print(5-2)
+# print(5/2)
+# print(5%2) #modulus operator
+# print(5**2)
+
+# Conditional Statements
+# eng_marks = 76
+
+# if eng_marks>90:
+# 	print("Grade: A+")
+# elif 80<eng_marks<=90:
+# 	print("Grade: A")
+# elif 70<eng_marks<=80:
+# 	print("Grade: B+")
+# else:
+# 	print("Fail")
+
+
+# num1 = 78
+# num2 = 43
+
+# if num1%num2 == 0:
+# 	print("Divisible")
+# else:
+# 	print("Not Divisible")
+
+#Logical Operator
+# print(True and True)
+# print(False and True)
+
+# print(True or True)
+# print(False or True)
+# print(False or False)
+
+# a = 48
+
+# if a%2 == 0:
+# 	if a%8 == 0:
+# 		print("Number is even and divisible by 8")
+# 	else:
+# 		print("Even")
+# else:
+# 	if a%7 == 0:
+# 		print("Number is odd and divisible by 7")
+# 	else:
+# 		print("Odd")
+
+# print(1,2,3,4,5,6,7,8,9,10)
+
+# for i in range(2,51,2):
+# 	print(i)
+
+# name = "Rishabh Shekhar"
+
+# for i in name:
+# 	print(i)
+
+# a = 4
+# for i in range(1,11):
+# 	print(i*a)
+
+# for i in range(2,100,2):
+# 	if i%7 == 0:
+# 		print(i)
+
+# STRING SLICING
+
+name = "Rishabh Shekhar"
+
+# print(name[-15])
+# print(name[4])
+# print(name[-1])
+# print(name[-2])
+
+# R 0 -15
+# i
+# s
+# h
+# a
+# b
+# h
+
+# S
+# h
+# e
+# k
+# h
+# a
+# r 14 -1
+
+# print(name[-4:])
+
+# print(len(name))
+
+
+# name ="Shashi Ranjan"
+# # print(len(name))
+# for i in range(0,len(name),2):
+#   print(name[i], end = " ")
+# print()
+
+#sep
+# a = 10
+# b = 20
+
+# 10!20
+
+# print(a,b, sep="!")
+# print(10)
+
+'''
+==
+<=
+>=
+!=
+'''
+# print(10!=12)
+
+print(5//2)
+
+# // --> floor division
+
+ac = "A Hey!@#$%^&*()_}{:L?>?<>?,./.;[]"
+print(len(ac))
+
+print(name[::-1])
+
+6! = 1*2*3*4*5*6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
