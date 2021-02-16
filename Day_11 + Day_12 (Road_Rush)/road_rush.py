b069d09c9fca2182a4192bb49ed98cc1ebfa55a3
RishabhShekhar <rishabhshekhar04@gmail.com>
2021-02-16 16:54:16 +0530

    Road

--- Day_11 + Day_12 (Road_Rush)/road_rush.py
+++ Day_11 + Day_12 (Road_Rush)/road_rush.py
@@ -1,2 +1,69 @@
 # imgs --> https://drive.google.com/drive/folders/1nI20V1rZaBBRcSNyy_KBxK908N-i9bgP?usp=sharing
 # sounds --> https://drive.google.com/drive/folders/1k8hZwgDzzcNAy1TFIWC0aZevAU8OL6Lj?usp=sharing
+
+import pygame
+import random
+
+pygame.init()
+
+screen_width = 640
+screen_height = 480
+half_screen_height = screen_height//2
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("ROAD RUSH")
+
+light_road = pygame.image.load("imgs/light_road.png")
+dark_road = pygame.image.load("imgs/dark_road.png")
+
+# Gradient
+texture_position = 0
+ddz = 0.002
+dz = 0
+z = 0
+
+road_pos = 0 # Our position on road
+road_accelaration = 80
+texture_position_acceleration = 4
+texture_position_threshold = 300
+half_texture_position_threshold = texture_position_threshold // 2
+
+
+running = True
+while running:
+    screen.fill((0, 0, 200))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            # pygame.quit()
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_UP]:
+        print("UP")
+        road_pos += road_accelaration
+        if road_pos >= texture_position_threshold:
+            road_pos = 0
+    if keys[pygame.K_DOWN]:
+        print("DOWN")
+        road_pos -= road_accelaration
+        if road_pos <= 0:
+            road_pos = texture_position_threshold
+
+    texture_position = road_pos
+    dz = 0
+    z = 0
+
+    for i in range(half_screen_height - 1, -1, -1):
+        if texture_position < half_texture_position_threshold:
+            screen.blit(light_road, (0, i + half_screen_height), (0, i, screen_width, 1))
+        else:
+            screen.blit(dark_road, (0, i + half_screen_height), (0, i, screen_width, 1))
+
+        dz += ddz
+        z += dz
+
+        texture_position += texture_position_acceleration + z
+        if texture_position >= texture_position_threshold:
+            texture_position = 0
+
+    pygame.display.update()
