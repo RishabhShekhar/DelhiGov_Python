c33b89f2c4558ccc3e02410ea3f7c040075bd1d6
RishabhShekhar <rishabhshekhar04@gmail.com>
2021-02-17 10:49:04 +0530

    Road Rush Blit

--- Day_11 + Day_12 (Road_Rush)/road_rush.py
+++ Day_11 + Day_12 (Road_Rush)/road_rush.py
@@ -8,12 +8,19 @@ pygame.init()
 
 screen_width = 640
 screen_height = 480
-half_screen_height = screen_height//2
+half_screen_height = screen_height // 2
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("ROAD RUSH")
 
 light_road = pygame.image.load("imgs/light_road.png")
 dark_road = pygame.image.load("imgs/dark_road.png")
+car = pygame.image.load("imgs/car.png")
+truck = pygame.image.load("imgs/truck.png")
+rock = pygame.image.load("imgs/rock.png")
+health = pygame.image.load("imgs/health.png")
+
+health_sound = pygame.mixer.Sound("sounds/health.wav")
+rock_sound = pygame.mixer.Sound("sounds/rock.wav")
 
 # Gradient
 texture_position = 0
@@ -21,12 +28,46 @@ ddz = 0.002
 dz = 0
 z = 0
 
-road_pos = 0 # Our position on road
+road_pos = 0  # Our position on road
 road_accelaration = 80
 texture_position_acceleration = 4
 texture_position_threshold = 300
 half_texture_position_threshold = texture_position_threshold // 2
 
+car_x = 260
+car_y = 360
+stone_x = random.randint(250, 350)
+stone_y = 240
+health_x = random.randint(250, 350)
+health_y = 240
+
+state = 0
+
+score = 0
+game_font = pygame.font.Font("freesansbold.ttf", 32)
+sCord = (10, 10)
+
+
+def print_score(scr):
+    screen.blit(game_font.render("SCORE: " + str(scr), True, (0, 0, 0)), sCord)
+
+
+def isCollided(Cx, Cy, Sx, Sy):
+    if Cx + 20 < Sx + 15 < Cx + 110 and Cy + 20 < Sy + 11 < Cy + 110:
+        return True
+    return False
+
+
+def draw_lives(l):
+    pygame.draw.rect(screen, (200, 0, 0), (600 - 30 * 4, 10, 30 * 5, 15))
+    for i in range(l):
+        pygame.draw.rect(screen, (0, 200, 0), (600 - 30 * i, 10, 30, 15))
+
+life = 5
+game = 1
+start_time = pygame.time.get_ticks()
+life_time = pygame.time.get_ticks()
+health_piece = 0
 
 running = True
 while running:
@@ -38,16 +79,20 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_UP]:
-        print("UP")
-        road_pos += road_accelaration
-        if road_pos >= texture_position_threshold:
-            road_pos = 0
-    if keys[pygame.K_DOWN]:
-        print("DOWN")
-        road_pos -= road_accelaration
-        if road_pos <= 0:
-            road_pos = texture_position_threshold
+    # if keys[pygame.K_UP]:
+    #     print("UP")
+    #     road_pos += road_accelaration
+    #     if road_pos >= texture_position_threshold:
+    #         road_pos = 0
+    # if keys[pygame.K_DOWN]:
+    #     print("DOWN")
+    #     road_pos -= road_accelaration
+    #     if road_pos <= 0:
+    #         road_pos = texture_position_threshold
+
+    road_pos += road_accelaration
+    if road_pos >= texture_position_threshold:
+        road_pos = 0
 
     texture_position = road_pos
     dz = 0
@@ -66,4 +111,8 @@ while running:
         if texture_position >= texture_position_threshold:
             texture_position = 0
 
+    screen.blit(car, (car_x, car_y))
+    screen.blit(truck, (270, 210))
+    print_score(score)
+    draw_lives(life)
     pygame.display.update()
